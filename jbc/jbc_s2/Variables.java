8d50166899c8e8eab1ef43ea8dc05e0cf2b12506
Catherine Loesch <catherine.loesch@protonmail.com>
2023-11-04 02:29:42 +0000

    section 2 completed

---
+++ jbc/jbc_s2/Variables.java
@@ -0,0 +1,14 @@
+class Variables {
+    public static void main(String[] args) {
+        int passengers = 5;
+        System.out.println(passengers);
+
+        int tickets = passengers;
+        System.out.println(tickets);
+
+        passengers = 13;
+        System.out.println(passengers);
+        System.out.println(tickets);
+
+    }
+}
